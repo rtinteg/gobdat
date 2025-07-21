c8cf6d144506f82cd835149848324e62e4fd33a5
Roberto Tinte García <rtinteg@gmail.com>
2025-07-21 20:30:38 +0000

    x

--- models/stg/stg_clientes.sql
+++ models/stg/stg_clientes.sql
@@ -1,9 +1,5 @@
-{{ config(
-    materialized = 'table'
-) }}
+-- {{ config(materialized="table") }}
 
-with stg_clientes as (
-    select *
-    from {{ source('stg', 'CUSTOMER') }}
-)
-select * from stg_clientes
+with stg_clientes as (select * from {{ source("stg", "CUSTOMER") }})
+select *
+from stg_clientes
