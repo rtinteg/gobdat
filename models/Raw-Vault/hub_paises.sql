a97a3f38e83f078c09796b2a61cb8171963162f5
Roberto Tinte García <rtinteg@gmail.com>
2025-07-22 16:58:51 +0000

    c

--- models/Raw-Vault/hub_paises.sql
+++ models/Raw-Vault/hub_paises.sql
@@ -1,12 +1,12 @@
 with
     hub_paises as (
         select
-            md5(upper(trim(nvl(m_name, '')))) as hub_pais_id,
+            md5(upper(trim(nvl(n_name, '')))) as hub_pais_id,
             n_name as nombre_pais,
             current_date as fecha_carga,
             'Snowflake' as origen
-        -- from {{ ref("stg_clientes")}}
-        from {{ source("stg","STG_PAISES")}}
+        
+        from {{ source("stg","STG_PAISES") }}
     )
 select *
 from hub_paises
