48faadbdff9b3b9e303aeb248f6ea424a17a722e
Roberto Tinte García <rtinteg@gmail.com>
2025-07-22 17:25:30 +0000

    f

---
+++ models/Raw-Vault/hub_proveedores.sql
@@ -0,0 +1,12 @@
+with
+    hub_proveedores as (
+        select
+            md5(upper(trim(nvl(s_name, '')))) as hub_proveedor_id,
+            s_name as nombre_proveedor,
+            current_date as fecha_carga,
+            'Snowflake' as origen
+        
+        from {{ source("stg","stg_p") }}
+    )
+select *
+from hub_proveedores
