a51ca86a8a1d03e4bd1594c96567fde9e862d9d6
Roberto Tinte García <rtinteg@gmail.com>
2025-07-22 13:59:01 +0000

    HUB

---
+++ models/Raw-Vault/hub_clientes.sql
@@ -0,0 +1,6 @@
+select 
+    md5(upper(trim(nvl(c_name,'')))) as HUB_cliente_id,
+    c_name as NOMBRE_CLIENTE,
+    current_date as FECHA_CARGA,
+    'Snowflake' as ORIGEN
+from {{ ref("stg_clientes")}}
