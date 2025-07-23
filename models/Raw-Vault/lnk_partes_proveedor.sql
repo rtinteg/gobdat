0e5f3682f5ac054a069f4c9db887edafb6e02389
Roberto Tinte García <rtinteg@gmail.com>
2025-07-23 08:08:16 +0000

    f

--- models/Raw-Vault/lnk_partes_proveedor.sql
+++ models/Raw-Vault/lnk_partes_proveedor.sql
@@ -1,24 +1,26 @@
 with
-    lnk_partes as (
+    hub_partes as (
         select
-            p_partkey,
-            p_name
-        
-        from {{ source("stg","STG_PARTES") }}
+            hub_parte_id,
+            nombre_parte        
+        from {{ source("raw","HUB_PARTES") }}
     )
-select *
-from lnk_partes,
-    lnk_proveedor as (
+,
+    hub_proveedor as (
         select
-            s_suppkey,
-            s_name
-        from {{ source("stg","STG_PROVEEDORES") }}
+            hub_proveedor_id,
+            nombre_proveedor
+        from {{ source("raw","HUB_PROVEEDORES") }}
     )
-select *
-from lnk_partes,
-    lnk_partes_proveedor as (
-        select 
-            ps_partkey,
-            ps_suppkey
-        from {{ source("stg","STG_PARTES_PROVEEDOR") }}
-    )
+
+select md5(upper(trim(nvl(p1.nombre_parte, ''))) || upper(trim(nvl(p2.nombre_proveedor, '')))) as lnk_partes_proveedor_id,
+    p1.hub_parte_id,
+    p2.hub_proveedor_id,
+    p1.nombre_parte,
+    p2.nombre_proveedor,
+    current_date as fecha_carga,
+    'Snowflake' as origen      
+from hub_partes p1,
+     hub_proveedor p2
+
+
