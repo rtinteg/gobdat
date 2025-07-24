f60d6c0b3dc39735fe6b06bc857bedd2ef86ca03
Roberto Tinte García <rtinteg@gmail.com>
2025-07-24 06:47:49 +0000

    g

--- models/Raw-Vault/hub_pedidos.sql
+++ models/Raw-Vault/hub_pedidos.sql
@@ -1,8 +1,11 @@
 with
     hub_pedidos as (
         select
-            md5(upper(trim(nvl(o_orderkey, '')))) as hub_pedido_id,
+            md5(
+                upper(trim(nvl(o_orderkey, ''))) || upper(trim(nvl(o_clerk, '')))
+            ) as hub_pedido_id,
             o_orderkey as clave_pedido,
+            o_clerk as empleado,
             current_date as fecha_carga,
             o_origen as origen
         from {{ source("stg", "STG_PEDIDOS") }}
--- models/Raw-Vault/lnk_lineas_pedidos.sql
+++ models/Raw-Vault/lnk_lineas_pedidos.sql
@@ -20,23 +20,32 @@ with
     ),
     stg_pedidos as (
         select
-            md5(upper(trim(nvl(o_orderkey, '')))) as hub_pedido_id,
-            o_orderkey as clave_pedido            
+            md5(
+                upper(trim(nvl(o_orderkey, ''))) || upper(trim(nvl(o_clerk, '')))
+            ) as pedido_id,
+            o_orderkey as clave_pedido,
+            o_clerk as empleado
         from {{ source("stg", "STG_PEDIDOS") }}
-        where o_clerk in ('Clerk#000000542','Clerk#000000081','Clerk#000000901') -- para reducir tiempos y limitar el volumen se filtra por empleado
-
+        where o_clerk in ('Clerk#000000542', 'Clerk#000000081', 'Clerk#000000901')  -- para reducir tiempos y limitar el volumen se filtra por empleado
     )
 select
     md5(
         upper(trim(nvl(p1.nombre_parte, '')))
         || upper(trim(nvl(p1.nombre_marca, '')))
-        || upper(trim(nvl(p2.nombre_proveedor, '')))
+        || upper(
+            trim(nvl(p2.nombre_proveedor, ''))
+            || upper(trim(nvl(p3.clave_pedido, '')))
+            || upper(trim(nvl(p3.empleado, '')))
+        )
     ) as lnk_partes_proveedor_id,
     p1.parte_id,
     p2.proveedor_id,
+    p3.pedido_id,
     p1.nombre_parte,
     p1.nombre_marca,
     p2.nombre_proveedor,
+    p3.clave_pedido,
+    p3.empleado,
     current_date as fecha_carga,
     'Snowflake' as origen
-from stg_partes p1, stg_proveedor p2
+from stg_partes p1, stg_proveedor p2, stg_pedidos p3
