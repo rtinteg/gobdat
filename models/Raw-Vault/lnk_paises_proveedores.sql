67e0de50519b9f68ce5fad509fd89ac9497b8cdd
Roberto Tinte García <rtinteg@gmail.com>
2025-07-24 16:27:29 +0000

    c

--- models/Raw-Vault/lnk_paises_proveedores.sql
+++ models/Raw-Vault/lnk_paises_proveedores.sql
@@ -6,21 +6,22 @@ with
         select hub_proveedor_id, nombre_proveedor
         from {{ source("raw", "HUB_PROVEEDORES") }}
     ),
-    stg_regiones_paises as (
-        select a.n_name, b.r_name
+    stg_regiones_proveedores as (
+        select a.n_name, b.s_name
         from {{ source("stg", "STG_PAISES") }} a
-        join {{ source("stg", "STG_REGIONES") }} b
-        where a.n_regionkey = b.r_regionkey
+        join {{ source("stg", "STG_PROVEEDORES") }} b
+        where a.n_nationkey = b.s_nationkey
     )
 select
     md5(
-        upper(trim(nvl(p1.nombre_region, ''))) || upper(trim(nvl(p2.nombre_pais, '')))
-    ) as lnk_regiones_paises_id,
-    p1.hub_region_id,
-    p2.hub_pais_id,
-    p1.nombre_region,
-    p2.nombre_pais,
+        upper(trim(nvl(p1.nombre_pais, '')))
+        || upper(trim(nvl(p2.nombre_proveedor, '')))
+    ) as lnk_regiones_proveedores_id,
+    p1.hub_pais_id,
+    p2.hub_proveedor_id,
+    p1.nombre_pais,
+    p2.nombre_proveedor,
     current_date as fecha_carga,
     'Snowflake' as origen
-from hub_regiones p1, hub_paises p2, stg_regiones_paises p3
-where p1.nombre_region = p3.r_name and p2.nombre_pais = p3.n_name
+from hub_paises p1, hub_proveedores p2, stg_regiones_proveedores p3
+where p1.nombre_pais = p3.n_name and p2.nombre_proveedor = p3.s_name
