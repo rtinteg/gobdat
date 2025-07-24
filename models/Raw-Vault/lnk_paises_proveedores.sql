ca6c6ee63e115a5cf99f4151db9eaa5f94c6fcd0
Roberto Tinte García <rtinteg@gmail.com>
2025-07-24 17:31:05 +0000

    v

--- models/Raw-Vault/lnk_paises_proveedores.sql
+++ models/Raw-Vault/lnk_paises_proveedores.sql
@@ -7,7 +7,7 @@ with
         from {{ source("raw", "HUB_PROVEEDORES") }}
     ),
     stg_paises_proveedores as (
-        select a.n_name, b.s_name
+        select a.n_name, b.s_name, a.n_origen, b.s_origen
         from {{ source("stg", "STG_PAISES") }} a
         join {{ source("stg", "STG_PROVEEDORES") }} b
         where a.n_nationkey = b.s_nationkey
@@ -22,6 +22,7 @@ select
     p1.nombre_pais,
     p2.nombre_proveedor,
     current_date as fecha_carga,
-    'Snowflake' as origen
+    n_origen as origen_pais,
+    s_origen as origen_proveedor
 from hub_paises p1, hub_proveedores p2, stg_paises_proveedores p3
 where p1.nombre_pais = p3.n_name and p2.nombre_proveedor = p3.s_name
