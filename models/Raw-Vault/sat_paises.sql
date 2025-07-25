59e016870ddb51f4c62ac85188d0d97b6bd9b413
Roberto Tinte García <rtinteg@gmail.com>
2025-07-25 07:17:59 +0000

    v

---
+++ models/Raw-Vault/sat_paises.sql
@@ -0,0 +1,15 @@
+with
+    sat_paises as (
+        select
+            b.hub_pais_id,
+            current_date as fecha_carga,
+            md5(
+                upper(trim(nvl(n_comment, ''))) || upper(trim(nvl(n_origen, '')))
+            ) as foto_cliente,
+            a.n_origen,
+            a.n_comment as comentario,
+        from {{ source("stg", "STG_PAISES") }} a
+        join {{ source("raw", "HUB_PAISES") }} b on a.n_name = b.nombre_pais
+    )
+select *
+from sat_paises
