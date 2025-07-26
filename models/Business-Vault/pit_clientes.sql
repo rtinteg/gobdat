b3e00848a8b7ccb347eea85f0f865c9ee5ca818f
Roberto Tinte García <rtinteg@gmail.com>
2025-07-26 05:58:16 +0000

    b

---
+++ models/Business-Vault/pit_clientes.sql
@@ -0,0 +1,30 @@
+select
+    h.hub_cliente_id,
+    current_date as pit_fecha,
+    s1.fecha_carga as fecha_cliente_contacto,
+    s2.fecha_carga as fecha_cliente_cuenta
+from {{ source("raw", "HUB_CLIENTES") }} h
+join
+    (
+        select *
+        from {{ source("raw", "SAT_CLIENTES_CONTACTO") }}
+        where
+            (hub_cliente_id, fecha_carga) in (
+                select hub_cliente_id, max(fecha_carga)
+                from {{ source("raw", "SAT_CLIENTES_CONTACTO") }}
+                group by hub_cliente_id
+            )
+    ) s1
+    on h.hub_cliente_id = s1.hub_cliente_id
+join
+    (
+        select *
+        from {{ source("raw", "SAT_CLIENTES_CUENTA") }}
+        where
+            (hub_cliente_id, fecha_carga) in (
+                select hub_cliente_id, max(fecha_carga)
+                from {{ source("raw", "SAT_CLIENTES_CUENTA") }}
+                group by hub_cliente_id
+            )
+    ) s2
+    on h.hub_cliente_id = s2.hub_cliente_id
