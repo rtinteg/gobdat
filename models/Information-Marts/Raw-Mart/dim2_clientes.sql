36a9a56354b62c763f11bb4df5cd03134ea36903
Roberto Tinte García <rtinteg@gmail.com>
2025-07-27 19:00:03 +0000

    d

--- models/Information-Marts/Raw-Mart/dim2_clientes.sql
+++ models/Information-Marts/Raw-Mart/dim2_clientes.sql
@@ -1,19 +1,16 @@
 select
     md5(
-        upper(trim(nvl(hc.cuenta_balance, '')))
-        || upper(trim(nvl(hc.segmento_marketing, '')))
-        || upper(
-            trim(nvl(hc.c_origen, '')) || to_char(sc.fecha_carga, 'YYYY-MM-DD')
-        ) as dim2_cliente_id
-    )
-    hc.cuenta_balance,
-    hc.segmento_marketing,
-    hc.c_origen as origen,
+        upper(trim(nvl(hc.nombre_cliente, ''))) || to_char(sc.fecha_carga, 'YYYY-MM-DD')
+    ) as dim2_cliente_id,
+    hc.nombre_cliente,
+    sc.cuenta_balance,
+    sc.segmento_marketing,
+    sc.c_origen as origen,
     sc.fecha_carga as fecha_actual,
     lead(sc.fecha_carga) over (
         partition by sc.hub_cliente_id order by sc.fecha_carga
     ) as fecha_final
 from
     {{ source("raw", "HUB_CLIENTES") }} hc,
-    {{ source("raw", "SAT_CLIENTES_CUENTAS") }} sc
+    {{ source("raw", "SAT_CLIENTES_CUENTA") }} sc
 where hc.hub_cliente_id = sc.hub_cliente_id
