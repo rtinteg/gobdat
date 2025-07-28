d339b8304b72b6f37ec13649bd29bc5a9712eb83
Roberto Tinte García <rtinteg@gmail.com>
2025-07-28 15:49:47 +0000

    c

--- models/Information-Marts/Raw-Mart/dim1_clientes.sql
+++ models/Information-Marts/Raw-Mart/dim1_clientes.sql
@@ -1,5 +1,10 @@
-select hc.hub_cliente_id, hc.nombre_cliente, sc.c_origen, sc.segmento_marketing
-from {{ source("raw", "HUB_CLIENTES") }} hc,
+select
+    hc.hub_cliente_id,
+    hc.nombre_cliente,
+    sc.segmento_marketing,
+    sc.fecha_carga as fecha,
+    sc.c_origen as origen
+from {{ source("raw", "HUB_CLIENTES") }} hc
 join
     {{ source("business", "PIT_CLIENTES") }} pit
     on hc.hub_cliente_id = pit.hub_cliente_id
@@ -7,3 +12,9 @@ left join
     {{ source("raw", "SAT_CLIENTES_CUENTA") }} sc
     on sc.hub_cliente_id = pit.hub_cliente_id
     and sc.fecha_carga = pit.fecha_cliente_cuenta
+where
+    sc.fecha_carga = (
+        select max(sc2.fecha_carga)
+        from {{ source("raw", "SAT_CLIENTES_CUENTA") }} sc2
+        where sc.hub_cliente_id = sc2.hub_cliente_id
+    )
