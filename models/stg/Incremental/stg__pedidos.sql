4a433a3f920e1d0a53dfcb7cad932474e2c5d113
Roberto Tinte García <rtinteg@gmail.com>
2025-07-29 17:58:32 +0000

    s

--- models/stg/Incremental/stg__pedidos.sql
+++ models/stg/Incremental/stg__pedidos.sql
@@ -7,15 +7,34 @@
         alias="stg_pedidos",
     )
 }}
+
 with
     csv_nuevos as (select * from {{ source("stg", "PEDIDOS_ELT") }}),
-    filtrados as (
-        select n.*
-        from csv_nuevos n
-        left join {{ ref("stg_pedidos") }} e
-        on n.o_orderkey = e.o_orderkey
-        where e.o_orderkey is null
-        -- where o_orderkey not in (select o_orderkey from {{ ref("stg_pedidos") }})
-    )
+
+    {% if not is_incremental() %}
+        filtrados as (
+            select n.*
+            from csv_nuevos n
+            left join {{ this }} e on n.o_orderkey = e.o_orderkey
+            where e.o_orderkey is null
+        )
+    {% else %} filtrados as (select * from csv_nuevos)
+    {% endif %}
+
 select *
-from filtrados
+from
+    filtrados
+
+    -- with
+    -- csv_nuevos as (select * from {{ source("stg", "PEDIDOS_ELT") }}),
+    -- filtrados as (
+    -- select n.*
+    -- from csv_nuevos n
+    -- left join {{ ref("stg_pedidos") }} e
+    -- on n.o_orderkey = e.o_orderkey
+    -- where e.o_orderkey is null
+    -- where o_orderkey not in (select o_orderkey from {{ ref("stg_pedidos") }})
+    -- )
+    -- select *
+    -- from filtrados
+    
