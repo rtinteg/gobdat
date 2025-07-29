df2550c4f1c4221a667799a11c0b8575959eecbe
Roberto Tinte García <rtinteg@gmail.com>
2025-07-29 20:24:35 +0000

    c

--- models/stg/Incremental/stg__pedidos.sql
+++ models/stg/Incremental/stg__pedidos.sql
@@ -7,15 +7,37 @@
         alias="stg_pedidos",
     )
 }}
-
 with
-    csv_nuevos as (select * from {{ source("stg", "PEDIDOS_ELT") }}),
+    csv_nuevos as (
+        select
+            c.o_orderkey,
+            c.o_custkey,
+            c.o_orderstatus,
+            c.o_totalprice,
+            c.o_orderdate,
+            c.o_orderpriority,
+            c.o_clerk,
+            c.o_shippriority,
+            c.o_comment,
+            c.o_origen
+        from {{ source("stg", "PEDIDOS_ELT") }} c
+    ),
     filtrados as (
-        select n.*
+        select
+            n.o_orderkey,
+            n.o_custkey,
+            n.o_orderstatus,
+            n.o_totalprice,
+            n.o_orderdate,
+            n.o_orderpriority,
+            n.o_clerk,
+            n.o_shippriority,
+            n.o_comment,
+            n.o_origen
         from csv_nuevos n
         left join {{ ref("stg_pedidos") }} e on n.o_orderkey = e.o_orderkey
-        --where e.o_orderkey is null
-       -- where o_orderkey not in (select o_orderkey from {{ ref("stg_pedidos") }})
+        where e.o_orderkey is null
+    -- where o_orderkey not in (select o_orderkey from {{ ref("stg_pedidos") }})
     )
 select *
 from filtrados
