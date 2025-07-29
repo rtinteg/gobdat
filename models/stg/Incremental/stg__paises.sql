2b2eef7fd51ef61c4241aae1ea1549c755a98e4a
Roberto Tinte García <rtinteg@gmail.com>
2025-07-29 16:58:55 +0000

    C

--- models/stg/Incremental/stg__paises.sql
+++ models/stg/Incremental/stg__paises.sql
@@ -10,11 +10,9 @@
 with
     csv_nuevos as (select * from {{ source("stg", "PAISES_ELT") }}),
     filtrados as (
-
         select *
         from csv_nuevos
         where n_nationkey not in (select n_nationkey from {{ ref("stg_paises") }})
-
     )
 select *
 from filtrados
