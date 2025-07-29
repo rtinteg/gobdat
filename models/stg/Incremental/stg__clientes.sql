58db563ab23df9b964235cdde279274f024be5e8
Roberto Tinte García <rtinteg@gmail.com>
2025-07-29 16:02:12 +0000

    d

---
+++ models/stg/Incremental/stg__clientes.sql
@@ -0,0 +1,54 @@
+{{ config(materialized="incremental", alias="stg_clientes_csv") }}
+
+with
+    csv_nuevos as (
+
+        select
+            c_acctbal,
+            c_address,
+            c_comment,
+            c_custkey,
+            c_mktsegment,
+            c_name,
+            c_nationkey,
+            c_phone,
+            c_origen,
+            current_timestamp() as fecha_carga
+        from {{ source("stg", "CLIENTES_ELT") }}
+
+    ),
+
+    filtrados as (
+
+        select *
+        from csv_nuevos
+        where c_custkey not in (select c_custkey from {{ ref("stg_clientes") }})
+
+    )
+
+    -- Esta tabla no se guarda como `load_clientes_from_csv`, sino que hace INSERT
+    -- INTO stg_clientes
+    insert into {{ ref("stg_clientes") }} (
+        c_acctbal,
+        c_address,
+        c_comment,
+        c_custkey,
+        c_mktsegment,
+        c_name,
+        c_nationkey,
+        c_phone,
+        c_origen,
+        fecha_carga
+    )
+select
+    c_acctbal,
+    c_address,
+    c_comment,
+    c_custkey,
+    c_mktsegment,
+    c_name,
+    c_nationkey,
+    c_phone,
+    c_origen,
+    fecha_carga
+from filtrados
