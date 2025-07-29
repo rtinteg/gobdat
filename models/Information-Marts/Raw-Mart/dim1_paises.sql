f19737a5d64e20bc5b5654ccdb666a9e8ddf094b
Roberto Tinte García <rtinteg@gmail.com>
2025-07-29 07:38:36 +0000

    v

--- models/Information-Marts/Raw-Mart/dim1_paises.sql
+++ models/Information-Marts/Raw-Mart/dim1_paises.sql
@@ -1,14 +1,23 @@
-select
-    hp.hub_pais_id as dim1_pais_id,
-    hp.nombre_pais,
-    sp.comentario,
-    sp.fecha_carga as fecha_actual,
-    sp.n_origen as origen
-from {{ source("raw", "HUB_PAISES") }} hp, {{ source("raw", "SAT_PAISES") }} sp
-where
-    hp.hub_pais_id = sp.hub_pais_id
-    and sp.fecha_carga = (
-        select max(fecha_carga)
-        from {{ source("raw", "SAT_PAISES") }} sp2
-        where hub_pais_id = sp2.hub_pais_id
+with
+    dim1_paises as (
+
+        select
+            rp.hub_pais_id as dim1_pais_id,
+            rp.hub_region_id as dim1_region_id,
+            rp.nombre_pais,
+            rp.nombre_region,
+            rp.fecha_carga as fecha_actual,
+            rp.origen_pais as origen
+        from
+            {{ source("raw", "LNK_REGIONES_PAISES") }} rp,
+            {{ source("raw", "SAT_PAISES") }} sp
+        where
+            rp.hub_pais_id = sp.hub_pais_id
+            and sp.fecha_carga = (
+                select max(fecha_carga)
+                from {{ source("raw", "SAT_PAISES") }} sp2
+                where rp.hub_pais_id = sp2.hub_pais_id
+            )
     )
+select *
+from dim1_paises
