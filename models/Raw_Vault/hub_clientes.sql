9422f30a2365ea741517d968f63026cd7c01c13f
Roberto Tinte García <rtinteg@gmail.com>
2025-07-30 08:23:13 +0000

    N

--- models/Raw_Vault/hub_clientes.sql
+++ models/Raw_Vault/hub_clientes.sql
@@ -1,11 +1,23 @@
+{{ config(materialized="incremental", unique_key="nombre_cliente") }}
+
 with
-    hub_clientes as (
-        select
-            md5(upper(trim(nvl(c_name, '')))) as hub_cliente_id,
-            c_name as nombre_cliente,
-            current_date as fecha_carga,
-            c_origen as origen
-        from {{ source("stg", "STG_CLIENTES") }}
+    hub_paises as (
+        {% if is_incremental() %}
+            select
+                md5(upper(trim(nvl(c_name, '')))) as hub_cliente_id,
+                c_name as nombre_cliente,
+                current_date as fecha_carga,
+                c_origen as origen
+            from {{ source("stg", "STG_CLIENTES") }}
+            where c_name not in (select nombre_cliente from {{ this }})
+        {% else %}
+            select
+                md5(upper(trim(nvl(c_name, '')))) as hub_cliente_id,
+                c_name as nombre_cliente,
+                current_date as fecha_carga,
+                c_origen as origen
+            from {{ source("stg", "STG_CLIENTES") }}
+        {% endif %}
     )
 select *
-from hub_clientes
+from hub_paises
