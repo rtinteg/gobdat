409d291bb0fa5cfcea68f92c7e6013dec28721fa
Roberto Tinte García <rtinteg@gmail.com>
2025-07-30 06:52:11 +0000

    B

--- models/stg/stg_clientes.sql
+++ models/stg/stg_clientes.sql
@@ -1,12 +1,34 @@
-with stg_clientes as (select * from {{ source("src", "CUSTOMER") }})
-select
-    c_acctbal,
-    c_address,
-    c_comment,
-    c_custkey,
-    c_mktsegment,
-    c_name,
-    c_nationkey,
-    c_phone,
-    'Snowflake' as c_origen
-from stg_clientes
+{{ config(materialized="incremental", unique_key="c_custkey") }}
+
+-- Selección condicional de la fuente de datos
+with
+    source_data as (
+        {% if is_incremental() %}
+            select
+                c_acctbal,
+                c_address,
+                c_comment,
+                c_custkey,
+                c_mktsegment,
+                c_name,
+                c_nationkey,
+                c_phone,
+                c_origen
+            from {{ source("stg", "CLIENTES_ELT") }}
+            where c_custkey not in (select c_custkey from {{ this }})
+        {% else %}
+            select
+                c_acctbal,
+                c_address,
+                c_comment,
+                c_custkey,
+                c_mktsegment,
+                c_name,
+                c_nationkey,
+                c_phone,
+                'Snowflake' as c_origen
+            from {{ source("src", "CUSTOMER") }}
+        {% endif %}
+    )
+select *
+from source_data
