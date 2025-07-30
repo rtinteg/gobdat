8c40a976a92836faca8de1e9793944eaca368636
Roberto Tinte García <rtinteg@gmail.com>
2025-07-30 11:35:47 +0000

    b

--- models/Mart_Vault/Raw_Mart/dim1_paises.sql
+++ models/Mart_Vault/Raw_Mart/dim1_paises.sql
@@ -1,3 +1,17 @@
+{{
+    config(
+        materialized="incremental",
+        unique_key="dim1_pais_id",
+        merge_update_columns=[
+            "dim1_region_id",
+            "nombre_pais",
+            "nombre_region",
+            "fecha_actual",
+            "origen",
+        ],
+    )
+}}
+
 with
     dim1_paises as (
 
@@ -8,16 +22,27 @@ with
             rp.nombre_region,
             rp.fecha_carga as fecha_actual,
             rp.origen_pais as origen
-        from
-            {{ source("raw", "LNK_REGIONES_PAISES") }} rp,
-            {{ source("raw", "SAT_PAISES") }} sp
+        from {{ source("raw", "LNK_REGIONES_PAISES") }} rp
+        join {{ source("raw", "SAT_PAISES") }} sp on rp.hub_pais_id = sp.hub_pais_id
         where
-            rp.hub_pais_id = sp.hub_pais_id
-            and sp.fecha_carga = (
-                select max(fecha_carga)
+            sp.fecha_carga = (
+                select max(sp2.fecha_carga)
                 from {{ source("raw", "SAT_PAISES") }} sp2
-                where rp.hub_pais_id = sp2.hub_pais_id
+                where sp2.hub_pais_id = sp.hub_pais_id
             )
+
+    ),
+    filtrado as (
+        select s.*
+        from dim1_paises s
+        left join
+            {{ this }} t
+            on s.dim1_pais_id = t.dim1_pais_id
+            and s.dim1_region_id = t.dim1_region_id
+            and s.nombre_pais = t.nombre_pais
+            and s.nombre_region = t.nombre_region
+            and s.origen = t.origen
+        where t.dim1_pais_id is null
     )
 select *
-from dim1_paises
+from filtrado
