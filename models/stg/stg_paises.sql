295ca7d5896b9b79ad3f4cb6339175e0e77e7960
Roberto Tinte García <rtinteg@gmail.com>
2025-07-30 06:47:03 +0000

    b

--- models/stg/stg_paises.sql
+++ models/stg/stg_paises.sql
@@ -1,3 +1,18 @@
-with stg_paises as (select * from {{ source("src", "NATION") }})
-select n_nationkey, n_name, n_regionkey, n_comment, 'Snowflake' as n_origen
-from stg_paises
+{{ config(materialized="incremental", unique_key="n_nationkey") }}
+
+-- Selección condicional de la fuente de datos
+with
+    source_data as (
+
+        {% if is_incremental() %}
+            select n_nationkey, n_name, n_regionkey, n_comment, n_origen
+            from {{ source("stg", "PAISES_ELT") }}
+            where n_nationkey not in (select n_nationkey from {{ this }})
+        {% else %}
+            select n_nationkey, n_name, n_regionkey, n_comment, 'Snowflake' as n_origen
+            from {{ source("src", "NATION") }}
+        {% endif %}
+
+    )
+select *
+from source_data
