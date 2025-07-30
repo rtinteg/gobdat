20dc359ba4c94b8695cc17137ffb0f658600c2f5
Roberto Tinte García <rtinteg@gmail.com>
2025-07-30 10:45:22 +0000

    F

--- models/Business_Vault/bridge_pedidos.sql
+++ models/Business_Vault/bridge_pedidos.sql
@@ -1,22 +1,39 @@
-select
-md5(
-        upper(trim(nvl(hp.clave_pedido, '')))
-        || upper(trim(nvl(hp.empleado, '')))
-        || upper(trim(nvl(hc.nombre_cliente, '')))
-        || upper(trim(nvl(hn.nombre_pais, '')))
-    ) as brigde_pedido_id,
-    current_date fecha_carga,
-    hp.hub_pedido_id,
-    hp.clave_pedido,
-    hp.empleado,
-    lcp.lnk_cliente_pedido_id,
-    hc.hub_cliente_id,
-    hc.nombre_cliente,
-    lpc.lnk_pais_cliente_id,
-    hn.hub_pais_id,
-    hn.nombre_pais
-from {{ source("raw", "HUB_PEDIDOS") }} hp
-join {{ ref("lnk_clientes_pedidos") }} lcp on hp.hub_pedido_id = lcp.hub_pedido_id
-join {{ source("raw", "HUB_CLIENTES") }} hc on hc.hub_cliente_id = lcp.hub_cliente_id
-join {{ ref('lnk_paises_clientes') }} lpc on lpc.hub_cliente_id = hc.hub_cliente_id
-join {{ source("raw", "HUB_PAISES") }} hn on hn.hub_pais_id = lpc.hub_pais_id
+{{ config(materialized="incremental", unique_key="bridge_pedido_id") }}
+
+with
+    bridge_pedido as (
+        select
+            md5(
+                upper(trim(nvl(hp.clave_pedido, '')))
+                || upper(trim(nvl(hp.empleado, '')))
+                || upper(trim(nvl(hc.nombre_cliente, '')))
+                || upper(trim(nvl(hn.nombre_pais, '')))
+            ) as bridge_pedido_id,
+            current_date fecha_carga,
+            hp.hub_pedido_id,
+            hp.clave_pedido,
+            hp.empleado,
+            lcp.lnk_cliente_pedido_id,
+            hc.hub_cliente_id,
+            hc.nombre_cliente,
+            lpc.lnk_pais_cliente_id,
+            hn.hub_pais_id,
+            hn.nombre_pais
+        from {{ source("raw", "HUB_PEDIDOS") }} hp
+        join
+            {{ source("raw", "LNK_CLIENTES_PEDIDOS") }} lcp
+            on hp.hub_pedido_id = lcp.hub_pedido_id
+        join
+            {{ source("raw", "HUB_CLIENTES") }} hc
+            on hc.hub_cliente_id = lcp.hub_cliente_id
+        join
+            {{ source("raw", "LNK_PAISES_CLIENTES") }} lpc
+            on lpc.hub_cliente_id = hc.hub_cliente_id
+        join {{ source("raw", "HUB_PAISES") }} hn on hn.hub_pais_id = lpc.hub_pais_id
+    )
+select *
+from bridge_pedido
+
+{% if is_incremental() %}
+    where bridge_pedido_id not in (select bridge_pedido_id from {{ this }})
+{% endif %}
