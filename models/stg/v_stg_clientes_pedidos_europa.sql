9bbebc78a319a1dc8d0d1f6e4edc393ab967435c
Roberto Tinte García <rtinteg@gmail.com>
2025-08-01 15:40:14 +0000

    v

--- models/stg/v_stg_clientes_pedidos_europa.sql
+++ models/stg/v_stg_clientes_pedidos_europa.sql
@@ -5,7 +5,7 @@ with
         select *
         from {{ source("stg", "STG_CLIENTES") }} a
         join {{ source("stg", "STG_PEDIDOS") }} b on a.c_custkey = b.o_custkey
-        join {{ ref("v_stg_regiones_paises") }} c on a.c_nationkey = c.n_regionkey
+        join {{ ref("v_stg_regiones_paises") }} c on a.c_nationkey = c.n_nationkey
     )
 select *
 from stg_clientes_pedidos_europa
