8e04a93a2405f9f8bbb7db7f9dda037f498fdd6c
Roberto Tinte García <rtinteg@gmail.com>
2025-08-01 15:05:11 +0000

    s

---
+++ models/stg/v_provee_partes_pedidos_europa.sql
@@ -0,0 +1,12 @@
+{{ config(materialized="view") }}
+
+with
+    stg_provee_partes_pedidos_europa as (
+        select *
+        from {{ source("stg", "STG_PROVEEDORES") }} a
+        join {{ source("stg", "STG_PARTES_PROVEEDOR") }} b on a.s_suppkey = b.ps_suppkey
+        join {{ source("stg", "STG_PARTES") }} c on b.ps_partkey = c.p_partkey
+        join {{ ref("v_stg_regiones_paises") }} d on a.s_nationkey = d.n_regionkey
+    )
+select *
+from stg_provee_partes_pedidos_europa
