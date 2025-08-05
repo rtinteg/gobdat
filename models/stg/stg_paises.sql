945411973653c45b77eedc5fd6d9078b6d36dde4
Roberto Tinte García <rtinteg@gmail.com>
2025-08-05 21:05:38 +0000

    v

--- models/stg/stg_paises.sql
+++ models/stg/stg_paises.sql
@@ -1,18 +1,33 @@
-{{ config(materialized="incremental", unique_key="n_nationkey") }}
+{{ config(materialized="incremental", unique_key=["n_nationkey", "load_date"]) }}
 
--- Selección condicional de la fuente de datos
+-- Fuente condicional de datos según tipo de carga
 with
     source_data as (
-
         {% if is_incremental() %}
-            select n_nationkey, n_name, n_regionkey, n_comment, n_origen
+
+            -- Incremental: datos nuevos desde CSV
+            select
+                n_nationkey,
+                n_name,
+                n_regionkey,
+                n_comment,
+                n_origen,
+                current_date as load_date
             from {{ source("stg", "PAISES_ELT") }}
-            where n_nationkey not in (select n_nationkey from {{ this }})
+
         {% else %}
-            select n_nationkey, n_name, n_regionkey, n_comment, 'Snowflake' as n_origen
+
+            -- Primera carga: desde tabla fuente intocable (sin origen ni fecha)
+            select
+                n_nationkey,
+                n_name,
+                n_regionkey,
+                n_comment,
+                'Snowflake' as n_origen,
+                current_date - 15 as load_date
             from {{ source("src", "NATION") }}
         {% endif %}
-
     )
+
 select *
 from source_data
