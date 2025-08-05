49575a037f69b16768f3013310138032fdc9bfe8
Roberto Tinte García <rtinteg@gmail.com>
2025-08-05 17:54:49 +0000

    c

--- models/Raw_Vault/sat_clientes_contacto.sql
+++ models/Raw_Vault/sat_clientes_contacto.sql
@@ -1,35 +1,29 @@
-{{ config(materialized="incremental", unique_key="hub_cliente_id") }}
+{{ config(materialized="incremental", unique_key=["hub_cliente_id", "load_date"]) }}
 
 with
     sat_clientes_contacto as (
         {% if is_incremental() %}
 
-            select
-                b.hub_cliente_id,
-                current_date as fecha_carga,
-                md5(
-                    upper(trim(nvl(c_address, '')))
-                    || upper(trim(nvl(c_comment, '')))
-                    || upper(trim(nvl(c_phone, '')))
-                    || upper(trim(nvl(c_origen, '')))
-                ) as foto_cliente,
-                a.c_origen,
-                a.c_address as direccion,
-                a.c_comment as comentario,
-                a.c_phone as telefono
-            from {{ source("stg", "STG_CLIENTES") }} a
-            join {{ source("raw", "HUB_CLIENTES") }} b on a.c_name = b.nombre_cliente
-            where hub_cliente_id not in (select hub_cliente_id from {{ this }})
+            where not exists (
+    select 1
+    from {{ this }} s
+    where s.hub_cliente_id = b.hub_cliente_id
+      and s.hashdiff = md5(
+        upper(trim(coalesce(a.c_address, ''))) ||
+        upper(trim(coalesce(a.c_comment, ''))) ||
+        upper(trim(coalesce(a.c_phone, '')))
+      )
+  )
+          
         {% else %}
             select
                 b.hub_cliente_id,
-                current_date as fecha_carga,
+                b.load_date as fecha_carga,
                 md5(
                     upper(trim(nvl(c_address, '')))
                     || upper(trim(nvl(c_comment, '')))
                     || upper(trim(nvl(c_phone, '')))
-                    || upper(trim(nvl(c_origen, '')))
-                ) as foto_cliente,
+                              ) as foto_cliente,
                 a.c_origen,
                 a.c_address as direccion,
                 a.c_comment as comentario,
@@ -40,3 +34,4 @@ with
     )
 select *
 from sat_clientes_contacto
+
