36abf617074024d25c14874314975b6cb1b310a9
Roberto Tinte García <rtinteg@gmail.com>
2025-08-05 21:26:55 +0000

    t

--- models/Raw_Vault/lnk_regiones_paises.sql
+++ models/Raw_Vault/lnk_regiones_paises.sql
@@ -5,7 +5,8 @@ with
         select hub_region_id, nombre_region from {{ source("raw", "HUB_REGIONES") }}
     ),
     hub_paises as (
-        select hub_pais_id, nombre_pais, from {{ source("raw", "HUB_PAISES") }}
+        select hub_pais_id, nombre_pais, fecha_carga
+        from {{ source("raw", "HUB_PAISES") }}
     ),
     stg_regiones_paises as (
         select a.n_name, b.r_name, a.n_origen, b.r_origen
@@ -23,7 +24,7 @@ with
             p2.hub_pais_id,
             p1.nombre_region,
             p2.nombre_pais,
-            current_date as fecha_carga,
+            p2.fecha_carga,
             p3.r_origen as origen_region,
             p3.n_origen as origen_pais
         from hub_regiones p1, hub_paises p2, stg_regiones_paises p3
