ff8ad06f3e8d0d0f9ce5554fc9cbcac6a9c0dc11
Roberto Tinte García <rtinteg@gmail.com>
2025-08-06 08:06:26 +0000

    v

--- models/stg/v_stg_clientes_pedidos_eu.sql
+++ models/stg/v_stg_clientes_pedidos_eu.sql
@@ -2,13 +2,44 @@
 
 with
     stg_clientes_pedidos_europa as (
-        select *
-        from {{ source("stg", "STG_CLIENTES") }} a
-        join {{ source("stg", "STG_PEDIDOS") }} b on a.c_custkey = b.o_custkey
+        select
+            c_acctbal,
+            c_address,
+            c_comment,
+            c_custkey,
+            c_mktsegment,
+            c_name,
+            c_nationkey,
+            c_phone,
+            c_origen,
+            a.load_date as fecha_cliente,
+            o_orderkey,
+            o_custkey,
+            o_orderstatus,
+            o_totalprice,
+            o_orderdate,
+            o_orderpriority,
+            o_clerk,
+            o_shippriority,
+            o_comment,
+            o_origen,
+            b.load_date as fecha_pedido,
+            r_regionkey,
+            r_name,
+            r_comment,
+            r_origen,
+            n_nationkey,
+            n_name,
+            n_regionkey,
+            n_comment,
+            n_origen,
+            c.load_date as fecha_pais
+        from sdgvaultmart.dbt_sdgvault.stg_clientes a
+        join sdgvaultmart.dbt_sdgvault.stg_pedidos b on a.c_custkey = b.o_custkey
         join
-            {{ ref("v_stg_regiones_paises") }} c
+            sdgvaultmart.dbt_sdgvault.v_stg_regiones_paises c
             on a.c_nationkey = c.n_nationkey
-            and c.n_nationkey in (6, 19, 22, 23, 99, 7)
+            and c.n_regionkey = 3
     )
 select *
 from stg_clientes_pedidos_europa
